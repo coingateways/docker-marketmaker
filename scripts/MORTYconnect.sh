8debdf35ba42dc5b38f7554c7e7d690d6a17f662
imylomylo <imylomylo@gmail.com>
2019-08-01 07:33:44 +0000

    silent curl minor edit

--- scripts/MORTYconnect.sh
+++ scripts/MORTYconnect.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /userpass.txt
-RESULT=$(curl --url "http://127.0.0.1:7783" --data "{\"userpass\":\"$userpass\",\"method\":\"electrum\",\"coin\":\"MORTY\",\"servers\":[{\"url\":\"electrum1.cipig.net:10018\"},{\"url\":\"electrum2.cipig.net:10018\"},{\"url\":\"electrum3.cipig.net:10018\"}]}" | jq '.')
+RESULT=$(curl -s --url "http://127.0.0.1:7783" --data "{\"userpass\":\"$userpass\",\"method\":\"electrum\",\"coin\":\"MORTY\",\"servers\":[{\"url\":\"electrum1.cipig.net:10018\"},{\"url\":\"electrum2.cipig.net:10018\"},{\"url\":\"electrum3.cipig.net:10018\"}]}" | jq '.')
 /usr/local/bin/telegram_send.sh "MORTY: ${RESULT}"
