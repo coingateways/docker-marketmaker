2e8783fd299d93f736d8961f3e8a38a20e52c00d
imylomylo <imylomylo@gmail.com>
2019-08-31 20:37:15 +1000

    cpl added scripts

--- scripts/RICKMORTYorderbook.sh
+++ scripts/RICKMORTYorderbook.sh
@@ -2,3 +2,4 @@
 source /userpass.txt
 RESULT=$(curl -s --url "http://127.0.0.1:7783" --data "{\"userpass\":\"$userpass\",\"method\":\"orderbook\",\"base\":\"RICK\",\"rel\":\"MORTY\"}" | jq '.')
 /usr/local/bin/telegram_send.sh "${RESULT}"
+echo ${RESULT}
